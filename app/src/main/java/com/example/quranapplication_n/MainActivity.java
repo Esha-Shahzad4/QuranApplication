db0634763baa19e3d2f5655759e8e826b4a74bbd
Muhammad Ghufran Ali <ghoofy.321@gmail.com>
2023-01-12 13:50:50 +0500

    github button added

--- app/src/main/java/com/example/quranapplication_n/MainActivity.java
+++ app/src/main/java/com/example/quranapplication_n/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.quranapplication_n;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     TextView aText;
     Button search;
     TextView display;
+    Button git;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         aText = findViewById(R.id.ayatText);
         search = findViewById(R.id.buttonSearch);
         display = findViewById(R.id.displayWindow);
+        git = findViewById(R.id.buttonGit2);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
                 String ayat=txt.QuranArabicText[ps];
                 display.setText(ayat);
 
+            }
+        });
+
+        git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "";
+
+                Uri webpage = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                startActivity(intent);
             }
         });
 
